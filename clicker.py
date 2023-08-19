350918a9e271ba25a74071dd51e8a1c5899e223a
Sebastian Savu <sebastian.savu@bidfx.com>
2023-08-19 09:26:38 +0100

    small cleanup

--- clicker.py
+++ clicker.py
@@ -1,9 +1,7 @@
-from ast import arg
-from pyautogui import *
 import pyautogui
-import time
 import keyboard
 import random
+import time
 import win32api
 import win32con
 
@@ -11,16 +9,21 @@ IMAGES = ["picture1.png","picture2.png","picture3.png"]
 CONFIDENCE = 0.8  # percentage
 PAUSE = 2  # seconds
 HOTKEYS = {}
-keys = ["q", "w", "e", "r"]
 
-KEY_FOR_IMAGE_RECOGNITION = 'q'
-KEY_FOR_REGISTERING_POSITION = 'w'
-KEY_FOR_AUTOMATED_CLICKING = 'e'
+KEY_FOR_REGISTERING_POSITION = '1'
+KEY_FOR_AUTOMATED_CLICKING = '2'
+KEY_FOR_IMAGE_RECOGNITION = '3'
+
+keys = [
+    KEY_FOR_REGISTERING_POSITION, 
+    KEY_FOR_AUTOMATED_CLICKING,
+    KEY_FOR_IMAGE_RECOGNITION 
+]
 
 REGISTERED_POSITIONS = []
 NR_OF_CLICKS = 1
-DELAY_1 = 0.1
-DELAY_2 = 1
+DELAY_BETWEEN_CLICKS = 0.1
+DELAY_BETWEEN_POINTS = 1
 DELAY_3 = 1
 DELAY_A = 0.5
 DELAY_B = 2
@@ -32,10 +35,6 @@ def toggleHotkey(key):
         del HOTKEYS[key]
     else:
         HOTKEYS[key] = True
-
-
-for key in keys:
-    keyboard.add_hotkey(key, toggleHotkey, args=(key))
 
 
 def position_and_click(x, y):
@@ -72,27 +71,35 @@ def image_based_recognition():
     time.sleep(DELAY_B)
 
 
-while 1:
+def init_keyboard_hotkeys():
+    for key in keys:
+        keyboard.add_hotkey(key, toggleHotkey, args=(key))
 
-    while KEY_FOR_AUTOMATED_CLICKING in HOTKEYS:
-        print("CLICKING...")
-        if not REGISTERED_POSITIONS:
-            print("NO REGISTERED CLICKS. PLEASE REGISTER CLICKS USING 'W'")
-            toggleHotkey(KEY_FOR_AUTOMATED_CLICKING)
 
-        for point in REGISTERED_POSITIONS:
-            for number in range(NR_OF_CLICKS):
-                position_and_click(point[0], point[1])
-                time.sleep(DELAY_1)
-            time.sleep(DELAY_2)
-        time.sleep(DELAY_3)
+def init_main_loop():
+    while 1:
+        while KEY_FOR_AUTOMATED_CLICKING in HOTKEYS:
+            if not REGISTERED_POSITIONS:
+                print("NO REGISTERED CLICKS. PLEASE REGISTER CLICKS USING 'W'")
+                toggleHotkey(KEY_FOR_AUTOMATED_CLICKING)
 
-    while KEY_FOR_REGISTERING_POSITION in HOTKEYS:
-        a = win32api.GetKeyState(0x01)
-        if a < 0:
-            (x, y) = getCursorPosition()
-            REGISTERED_POSITIONS.append((x, y))
-            toggleHotkey(KEY_FOR_REGISTERING_POSITION)
-            print(REGISTERED_POSITIONS)
-    while KEY_FOR_IMAGE_RECOGNITION in HOTKEYS:
-        image_based_recognition()
+            print("CLICKING...")
+            for (x,y) in REGISTERED_POSITIONS:
+                for _ in range(NR_OF_CLICKS):
+                    position_and_click(x, y)
+                    time.sleep(DELAY_BETWEEN_CLICKS)
+                time.sleep(DELAY_BETWEEN_POINTS)
+            time.sleep(DELAY_3)
+
+        while KEY_FOR_REGISTERING_POSITION in HOTKEYS:
+            a = win32api.GetKeyState(0x01)
+            if a < 0:
+                (x, y) = getCursorPosition()
+                REGISTERED_POSITIONS.append((x, y))
+                toggleHotkey(KEY_FOR_REGISTERING_POSITION)
+                print(REGISTERED_POSITIONS)
+        while KEY_FOR_IMAGE_RECOGNITION in HOTKEYS:
+            image_based_recognition()
+
+init_keyboard_hotkeys()
+init_main_loop()
